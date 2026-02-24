a8872d95909f76c8b01c21c666d5ee0ed4adb15c
Aditi Saptarishy <saptarishyaditi@gmail.com>
2026-02-24 16:48:11 -0700

    modified code

--- orchestra-test-workflows/process_task.py
+++ orchestra-test-workflows/process_task.py
@@ -1,14 +1,6 @@
-import json
-import sys
-
 def main():
-    # Get input passed as argument
-    input_data = sys.argv[1]
-
-    data = json.loads(input_data)
-
-    print("Received from Task 1:", data)
-    print("Number * 2 =", data["number"] * 2)
+    print("Task 2 executed successfully.")
+    print("Simulated processing.")
 
 if __name__ == "__main__":
     main()
